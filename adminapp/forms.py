9bc3427fe0a70d052b65566c349de0074c9374a1
SREYA VEERAPANENI <2300030976@kluniversity.in>
2024-11-04 13:23:30 +0530

    hello

---
+++ adminapp/forms.py
@@ -0,0 +1,26 @@
+import pytz
+from django import forms
+from .models import Task, StudentList
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields=['title']
+
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentList
+        fields = ['Register_Number', 'Name']
+
+
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone_number', 'address']
+
---
+++ studentapp/views.py
@@ -0,0 +1,24 @@
+
+from django.shortcuts import render
+def homepage(request):
+    return render(request ,'HomePage.html')
+def StudentHomePage(request):
+    return render(request, 'studentapp/StudentHomePage.html')
+
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from facultyapp.models import Marks
+from adminapp.models import StudentList
+def view_marks(request):
+    user=request.user
+    
+    try:
+        student_user=User.objects.get(username=user.username)
+        student=StudentList.objects.get(Register_Number=student_user)
+        marks=Marks.objects.filter(student=student)
+        return render(request,'studentapp/view_marks.html',{'marks':marks})
+    except (StudentList.DoesNotExist,user.DoesNotExist):
+        return render(request,'studentapp/no_studentlist.html',{
+        'error':'No Student record found for this user.'
+        })
+
